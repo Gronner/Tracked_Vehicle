43b935b97c44b31d639f43bd1a56df7c38bc938a
Gronner <Gronner@users.noreply.github.com>
2018-03-09 18:36:36 +0100

    Minimal compileable and flashable working example with two FreeRTOS tasks

--- src/program.c
+++ src/program.c
@@ -1,20 +1,73 @@
 #include <stdint.h>
+#include "FreeRTOS.h"
+#include "task.h"
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_gpio.h"
 
 int main(void);
 void vApplicationMallocFailedHook (void);
 void vApplicationStackOverflowHook(void);
+void create_tasks(void);
+void Task1(void * pvParameters);
+void Task2(void * pvParameters);
 
 int main(void){
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
     __asm("dsb");
     GPIOD->MODER |= (1 << (GPIO_PinSource14 * 2));
-    uint32_t i = 0;
+    GPIOD->MODER |= (1 << (GPIO_PinSource12 * 2));
+
+    create_tasks();
+
+    vTaskStartScheduler();
+
+    for(;;);
+
+    return 0;
+}
+
+void create_tasks(void){
+    xTaskCreate(Task1,
+                "Task1",
+                100,
+                NULL,
+                1,
+                NULL);
+    xTaskCreate(Task2,
+                "Task2",
+                100,
+                NULL,
+                1,
+                NULL);
+}
+
+void Task1(void * pvParameters){
+    TickType_t xLastWakeTime;
+
+    xLastWakeTime = xTaskGetTickCount();
+
     for(;;){
-        GPIOD->ODR ^= (1 << GPIO_PinSource14);
-        for(i = 0; i<UINT16_MAX*5; i++){}
+        GPIOD->ODR ^= (1 << (GPIO_PinSource14));
+
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(250));
     }
+
+    vTaskDelete(NULL);
+}
+
+void Task2(void* pvParameters){
+
+    TickType_t xLastWakeTime;
+
+    xLastWakeTime = xTaskGetTickCount();
+
+    for(;;){
+        GPIOD->ODR ^= (1 << (GPIO_PinSource12));
+
+        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(250));
+    }
+
+    vTaskDelete(NULL);
 }
 
 void vApplicationMallocFailedHook(void)
