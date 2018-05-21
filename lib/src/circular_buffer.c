961f7741de3108324a06a8c01bb954503f6f1196
Gronner <Gronner@users.noreply.github.com>
2018-05-21 17:17:28 +0200

    Cleanup circualar buffer for refecatroing

--- lib/src/circular_buffer.c
+++ lib/src/circular_buffer.c
@@ -10,29 +10,7 @@ void circ_buffer_reset(circ_buffer_t* buffer){
 }
 
 void circ_buffer_add_value(circ_buffer_t* buffer, uint16_t value){
-    buffer->memory[buffer->head] = value;
-    buffer->head++;
-    if(buffer->head == buffer->tail){
-        buffer->tail++;
-        if(!(buffer->tail < BUFFER_SIZE)){
-            buffer->tail = 0;
-        }
-    }
-    if(!(buffer->head < BUFFER_SIZE)){
-        buffer->head = 0;
-    }
 }
 
 uint8_t circ_buffer_read_value(circ_buffer_t* buffer, uint16_t* value){
-    if(buffer->tail == buffer->head - 1){
-        return 1;
-    }
-    else{
-        buffer->tail++;
-        if(!(buffer->tail < BUFFER_SIZE)){
-            buffer->tail = 0;
-        }
-        *value = buffer->memory[buffer->tail];
-    }
-    return 0;
 }
